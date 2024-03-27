9325e4167d005c04afa0b11e76f28275a41a9e21
Apple <apple@Aana-MacBook-Pro.local>
2024-03-27 21:10:21 +0600

    component

--- src/components/Weather-counrty/WeatherCountry.jsx
+++ src/components/Weather-counrty/WeatherCountry.jsx
@@ -1,9 +1,31 @@
 import  cls from './weatherCountry.module.css'
+import textLike from '../../assets/textLike.svg'
+import les from '../../assets/les.svg'
+import autumn from '../../assets/autumn.svg'
+import summer from '../../assets/summer.svg'
+import palm from '../../assets/palm.svg'
 
 const WeatherCountry = () => {
   return (
     <div>
-        <div className={cls.country__wrap}></div>
+        <div className={cls.country__wrap}> 
+        <div className={cls.textLikes}>
+          <img className={cls.Like} src={textLike} alt="" />
+          <h2 className={cls.text}>Activities in your area</h2>
+        </div>
+        <div className={cls.pic}>
+          <img className={cls.nature} src={les} alt="" />
+          <img className={cls.nature} src={autumn} alt="" />
+          <img className={cls.nature} src={summer} alt="" />
+          <img className={cls.nature} src={palm} alt="" />
+        </div>
+        <div className={cls.aw}>
+           <h3 className={cls.away}>2km away</h3>
+           <h3 className={cls.away}>1.5km away</h3>
+           <h3 className={cls.away}>3km away</h3>
+           <h3 className={cls.away}>500m away</h3>
+        </div>
+        </div>
         <div className={cls.country__wrap}></div>
     </div>
   )
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,22 @@
 import cls from './sidebar.module.css'
+import icon from '../../assets/icon.svg'
+import wt from '../../assets/wt.svg'
+import ex from '../../assets/ex.svg'
+import ci from '../../assets/ci.svg'
+import se from '../../assets/se.svg'
 
 const Sidebar = () => {
   return (
     <div className={cls.sidebar}>
-        
+      <div>
+         <img src={icon} alt="" />
+      </div>
+      <div className={cls.menu}>
+          <img className={cls.icmenu} src={wt} alt="" />
+          <img className={cls.icmenu} src={ex} alt="" />
+          <img className={cls.icmenu} src={ci} alt="" />
+          <img className={cls.icmenu} src={se} alt="" />
+      </div>
     </div>
   )
 }
